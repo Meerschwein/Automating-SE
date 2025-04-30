89909aaa315c3158c1f302546918b22fb2d3b2df
Merlin Volkmer <merlin.volkmer@outlook.com>
2025-04-30 18:10:24 +0200

    upload Merlin Checkpoint 1

--- shell.nix
+++ shell.nix
@@ -3,10 +3,16 @@ let
 in
   pkgs.mkShell {
     buildInputs = with pkgs; [
+      ruff
       (python3.withPackages (ps:
         with ps; [
           pandas
           scikit-learn
+          transformers
+          torch
+          datasets
+          evaluate
+          accelerate
         ]))
     ];
   }
